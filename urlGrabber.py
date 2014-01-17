7370c5ce9754c247a7dd5f6ab9004d496d5c4d77
rebeccamurphy <rebeccalorenmurphy@gmail.com>
2014-01-17 15:47:41 -0500

    added comments.

--- urlGrabber.py
+++ urlGrabber.py
@@ -1,10 +1,8 @@
 import urllib2
-'''
-with open("pagesource.txt", "w") as text_file:
-    text_file.write(str.format(page_source))
 
-'''
 baseURL = "http://library.marist.edu/archives/LTP/" # home page archive
+startURL = "http://library.marist.edu/archives/LTP/LTP.xml" # seed url 
+
 
 def pagecrawl(url, space1, space2, section):
 	response = urllib2.urlopen(url) #opens the url
@@ -20,24 +18,20 @@ def pagecrawl(url, space1, space2, section):
 			if section != "":
 				childURL = baseURL + section +"/" +endURL # adds subSeries with section if it has a section
 			else:
-				childURL = baseURL +endURL
+				childURL = baseURL +endURL # url is just a series, so it has no section
 				sectionsURL.append(childURL)
 			listURL.append(childURL)
 			
-startURL = "http://library.marist.edu/archives/LTP/LTP.xml"
 
-sectionsURL =[] 
-listURL = []
-listLen =0
+sectionsURL =[]  #list of all the series urls
+listURL = []	#complete list of urls, including all the series subseries
 
-pagecrawl(startURL, 0, 0, "")
+pagecrawl(startURL, 0, 0, "") # starts the page crawl with the start url, starting spaces being the beginning of the document. originally only gets the series urls.
 
 for sectionURL in sectionsURL:
-	
-	pagecrawl(sectionURL, 0, 0,sectionURL.split('/')[len(sectionURL.split()) -3])
+	pagecrawl(sectionURL, 0, 0,sectionURL.split('/')[len(sectionURL.split()) -3]) # this loop gets each subseries in a series
 
-
-with open("Output.txt", "w") as text_file:
+with open("Output.txt", "w") as text_file: # writes out the urls to output.txt 
     for item in listURL:
     	text_file.write(str.format("%s\n" % item))
 
